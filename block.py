445607c451b70703aac16f76ede520c0c55e7c26
73F <mxl73@outlook.com>
2017-08-04 02:30:45 +0800

    = export block class

---
+++ block.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Block:
+    def __init__(self, position):
+        self.image = pygame.image.load("images/block.png")
+        self.alive = True
+        self.x = position[0]
+        self.y = position[1]
+        rect = self.image.get_rect()
+        self.width = rect.right - rect.left
+        self.height = rect.bottom - rect.top
+
+    def kill(self):
+        self.alive = False
+
+    def size(self):
+        return [self.width, self.height]
+
+    def position(self):
+        return [self.x, self.y]
+
+    def collide(self, o):
+        return self.alive and (rect_intersects(self, o) or rect_intersects(o, self))
